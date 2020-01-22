bc2331908d28d918a940773e0f31e7b21e042dfd
Harshul <harshul@editorialistyx.com>
2020-01-22 13:12:09 +0530

    sdn commit

--- index.js
+++ index.js
@@ -1,6 +1,7 @@
 var fs = require('fs');
 var count = require('./stuff');
 fs.writeFileSync('output.txt',count([1,2,3,4]));
+//commit for sdn branch
 
 console.log('making changes for online repo in local server');
 

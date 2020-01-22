29ef74dfc632726a6642acfec7311fa3c583342c
Harshul <harshul@editorialistyx.com>
2020-01-22 13:13:21 +0530

    auth commit

--- index.js
+++ index.js
@@ -1,7 +1,7 @@
 var fs = require('fs');
 var count = require('./stuff');
 fs.writeFileSync('output.txt',count([1,2,3,4]));
-
+// auth commit
 console.log('making changes for online repo in local server');
 
 console.log('Auth commit');

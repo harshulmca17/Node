307d71c9d1d15dc48c43bd24767addcd1fb0c7b7
Harshul <harshul@editorialistyx.com>
2020-01-22 10:52:01 +0530

    Initial local commit

--- index.js
+++ index.js
@@ -2,6 +2,8 @@ var fs = require('fs');
 var count = require('./stuff');
 fs.writeFileSync('output.txt',count([1,2,3,4]));
 
+console.log('making changes for online repo in local server');
+
 console.log('Auth commit');
 setTimeout(function(){
     console.log('jkhjkh');
